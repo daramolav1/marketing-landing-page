945bd96ccf9adec2509beb3c567353a2ab6d03da
Victor <victorddaramola@gmail.com>
2022-08-31 19:26:00 -0400

    Sidebar component with various styling

---
+++ src/components/Sidebar/SidebarContent.js
@@ -0,0 +1,89 @@
+import styled from "styled-components";
+import { FaTimes } from "react-icons/fa";
+import { Link as LinkR } from "react-router-dom";
+import { Link as LinkS } from "react-scroll";
+
+export const SidebarContainer = styled.aside`
+  display: grid;
+  align-items: center;
+  position: fixed;
+  top: ${({ isOpen }) => (isOpen ? "0" : "-100%")};
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #0d0d0d;
+  opacity: ${({ isOpen }) => (isOpen ? "100%" : "0")};
+  z-index: 999;
+  transition: 0.3s ease-in-out;
+`;
+
+export const CloseIcon = styled(FaTimes)`
+  color: #fff;
+`;
+
+export const Icon = styled.div`
+  position: absolute;
+  top: 1.2rem;
+  right: 1.5rem;
+  background: transparent;
+  font-size: 2rem;
+  cursor: pointer;
+  outline: none;
+`;
+
+export const SidebarWrapper = styled.div`
+  color: #fff;
+`;
+
+export const SidebarMenu = styled.ul`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: repeat(6, 80px);
+  text-align: center;
+
+  @media screen and (max-width: 400px) {
+    grid-template-rows: repeat(6, 60px);
+  }
+`;
+
+export const SidebarLink = styled(LinkS)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #fff;
+  font-size: 1.5rem;
+  list-style: none;
+  text-decoration: none;
+  cursor: pointer;
+  transition: 0.2s ease-in-out;
+
+  &:hover {
+    color: #01bf71;
+    transition: 0.2s ease-in-out;
+  }
+`;
+
+export const SideBtnWrap = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const SidebarRoute = styled(LinkR)`
+  border: none;
+  border-radius: 50px;
+  padding: 16px 64px;
+  white-space: nowrap;
+  background-color: #01bf71;
+  color: #010606;
+  font-size: 16px;
+  text-decoration: none;
+  outline: none;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+
+  &:hover {
+    background-color: #fff;
+    color: #010606;
+    transition: all 0.2s ease-in-out;
+  }
+`;
