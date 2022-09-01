5036a4e711df52398eb072c52f2e77a411868552
Victor <victorddaramola@gmail.com>
2022-09-01 01:18:51 -0400

    Added different routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages";
+import SignInPage from "./pages/signin";
 
 function App() {
   return (
     <Router>
-      <Home />
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/signin" exact element={<SignInPage />} />
+      </Routes>
     </Router>
   );
 }
