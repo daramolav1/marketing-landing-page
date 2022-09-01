493ee9d98c1e2baa2a5b263409eb2b6a3e4b510a
Victor <victorddaramola@gmail.com>
2022-09-01 00:00:50 -0400

    Created data objects for reusable component

---
+++ src/components/Info/Data.js
@@ -0,0 +1,57 @@
+import svg1 from "../../images/svg-1.svg";
+import svg2 from "../../images/svg-2.svg";
+import svg3 from "../../images/svg-3.svg";
+
+export const infoObj1 = {
+  id: "about",
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  dark: true,
+  darkText: false,
+  primary: true,
+  topLine: "Premium Bank",
+  headline: "Unlimited transactions with zero fees",
+  description:
+    "Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.",
+  buttonLabel: "Get Started",
+  imgStart: false,
+  img: svg1,
+  alt: "Car",
+};
+
+export const infoObj2 = {
+  id: "discover",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  dark: false,
+  darkText: true,
+  primary: false,
+  topLine: "Unlimited Access",
+  headline: "Login to your account at any time",
+  description:
+    "We have you covered no matter where you are located. All you need is an internet connection and a phone or computer.",
+  buttonLabel: "Learn More",
+  imgStart: true,
+  img: svg2,
+  alt: "Bank",
+};
+
+export const infoObj3 = {
+  id: "signup",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  dark: false,
+  darkText: true,
+  primary: false,
+  topLine: "Join Our Team",
+  headline: "Creating an account is extremely easy",
+  description:
+    "Get everything set up and ready in under 10 minutes. All you need to do is add your info and you're ready to go.",
+  buttonLabel: "Sign Up",
+  imgStart: false,
+  img: svg3,
+  alt: "Chart",
+};
